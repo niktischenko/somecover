0cec77604383e1a333abd149d748c307ec193ba1
Nikolay Tischenko <somebody@somebody-laptop.(none)>
2011-03-16 22:07:43 +0600

    debianized + changelog

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the somecover package
+#
+0 4	* * *	root	somecover_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(somecover):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="somecover" command="/usr/bin/somecover"
